fd01a5337d9b571d10eb64a5c1e4437d0ffeecd3
Alexander Skrabl <alexander@skrabl.de>
2016-03-04 23:16:51 +0000

    dev set up updates and added react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('hello world');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Slider from './slider.jsx';
+
+ReactDOM.render( <Slider />, document.getElementById('Slider') );
---
+++ src/slider.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Slider extends React.Component {
+    constructor ( props ) {
+        super( props );
+    }
+
+    render() {
+        return (
+            <div>
+                Slide Me Slider
+            </div>
+        )
+    }
+}
+
+export default Slider;
