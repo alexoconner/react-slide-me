c4d042f4bbc0fcea0ba29b8fd86d3ee69ec6840d
Alexander Skrabl <alexander@skrabl.de>
2016-03-06 01:43:12 +0000

    moved styles into styles object and some renaming

--- src/slider.jsx
+++ src/slider.jsx
@@ -35,13 +35,12 @@ class Slider extends React.Component {
     componentDidMount() {
         const { animation } = this.props;
         const sliderItems = document.querySelectorAll('.slider-item');
-        console.log(sliderItems.length);
+
         for ( let i = 0; i < sliderItems.length; i++ ) {
             let item = sliderItems[i];
             switch ( animation ) {
                 case 'fade':
-                    if ( item.classList.contains('first') ) {
-                        console.log('first found');
+                    if ( item.classList.contains('active') ) {
                         item.style.opacity = 1;
                     }
                     break;
@@ -58,45 +57,44 @@ class Slider extends React.Component {
 
         const { items, size, type, speed, animation } = this.props;
 
-        const container = {
-            position: 'relative',
-            display: 'block',
-            width: size.width,
-            height: size.height
-        }
-
-        const containerItem = {
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: size.width,
-            height: size.height,
-            zIndex: 100,
-            transition: 'all ' + speed + 'ms',
-        }
-
-        const btnPrevious = {
-            position: 'absolute',
-            left: '10px',
-            top: '50%',
-            transform: 'translateY(-50%)',
-            zIndex: '99999'
+        const styles = {
+            container: {
+                position: 'relative',
+                display: 'block',
+                width: size.width,
+                height: size.height
+            },
+            containerItem: {
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: size.width,
+                height: size.height,
+                zIndex: 100,
+                transition: 'all ' + speed + 'ms',
+            },
+            btnPrevious: {
+                position: 'absolute',
+                left: '10px',
+                top: '50%',
+                transform: 'translateY(-50%)',
+                zIndex: '99999'
+            },
+            btnNext: {
+                position: 'absolute',
+                right: '10px',
+                top: '50%',
+                transform: 'translateY(-50%)',
+                zIndex: '99999'
+            }
         }
 
         switch ( animation ) {
             case 'fade':
-                containerItem.opacity = 0;
+                styles.containerItem.opacity = 0;
                 break;
             default:
-                containerItem.opacity = 0;
-        }
-
-        const btnNext = {
-            position: 'absolute',
-            right: '10px',
-            top: '50%',
-            transform: 'translateY(-50%)',
-            zIndex: '99999'
+                styles.containerItem.opacity = 0;
         }
 
         let mapedItems = [];
@@ -105,19 +103,19 @@ class Slider extends React.Component {
                 let itemClass = 'slider-item';
 
                 if ( index === 0 ) {
-                    itemClass = itemClass + ' first';
+                    itemClass = itemClass + ' active';
                 }
 
                 if ( typeof item === 'string' ) {
                     return (
-                        <div key={ index } ref="slider-item" style={ containerItem } className={ itemClass }>
+                        <div key={ index } ref="slider-item" style={ styles.containerItem } className={ itemClass }>
                             <img key={ index } src={ item } alt={ item } />
                         </div>
                     )
                 }
                 else if ( typeof item === 'object' ) {
                     return (
-                        <div key={ index } ref="slider-item" style={ containerItem } className={ itemClass }>
+                        <div key={ index } ref="slider-item" style={ styles.containerItem } className={ itemClass }>
                             <img src={ item.url } alt={ item.alt } />
                         </div>
                     )
@@ -126,9 +124,9 @@ class Slider extends React.Component {
         }
 
         return (
-            <div ref="slide-me-container" style={ container }>
-                <button style={ btnPrevious } onClick={ this.btnPrevious }>previous</button>
-                <button style={ btnNext } onClick={ this.btnNext }>next</button>
+            <div ref="slide-me-container" style={ styles.container }>
+                <button style={ styles.btnPrevious } onClick={ this.btnPrevious }>previous</button>
+                <button style={ styles.btnNext } onClick={ this.btnNext }>next</button>
                 { mapedItems }
             </div>
         )
