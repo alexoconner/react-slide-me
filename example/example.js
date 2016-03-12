21b4c807533ae08323a8f823be4f683024e71068
Alexander Skrabl <alexander@skrabl.de>
2016-03-12 22:19:40 +0000

    some refactoring to take example out of src//lib

---
+++ example/example.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Slider from '../src/index.js';
+
+var items = [
+    'sample-images/01.jpg',
+    'sample-images/02.jpg',
+    {
+        url: 'sample-images/03.jpg',
+        alt: 'New York Skyline',
+        title: 'Photo of New York skyline taken in Brooklyn'
+    },
+    'sample-images/04.jpg',
+    'sample-images/05.jpg'
+];
+
+var size = {
+    width: 480,
+    height: 320
+}
+
+ReactDOM.render( <Slider items={ items } type="image" size={ size } animation="fade" speed={ 1000 } easing="ease" />, document.getElementById('SliderFade') );
+ReactDOM.render( <Slider items={ items } type="image" size={ size } animation="moveHorizontal" speed={ 1000 } easing="ease" />, document.getElementById('SliderMove') );
