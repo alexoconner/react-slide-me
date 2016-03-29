d015544211b27b450825f19144510b1b5c9de754
Alexander Skrabl <alexander@skrabl.de>
2016-03-29 20:40:37 +0100

    dev env updates.. still a bit buggy

---
+++ src/test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Slider from './index.js';
+
+var items = [
+    'sample-images/01.jpg',
+    'sample-images/02.jpg',
+    {
+        url: 'sample-images/03.jpg',
+        alt: 'New York Skyline',
+        title: 'Photo of New York skyline taken in Brooklyn'
+    },
+    'sample-images/04.jpg',
+    'sample-images/05.jpg'
+];
+
+var size = {
+    width: 480,
+    height: 320
+}
+
+ReactDOM.render( <Slider items={ items } type="image" size={ size } animation="fade" speed={ 1000 } easing="ease" />, document.getElementById('SliderFade') );
